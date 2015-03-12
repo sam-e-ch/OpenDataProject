0c2b00b7cbb52ca535cc5033bebfe3dcfeb51f92
GodlikeSpam0r <babemusicstar@hotmail.com>
2015-03-12 10:50:14 +0100

    Tweaked dataParser
    Lines without departuretime removed

--- dataParser/DataParser.java
+++ dataParser/DataParser.java
@@ -6,22 +6,25 @@ public class DataParser{
 		try {
 			File file = new File("data/FPLAN");
 			BufferedReader br = new BufferedReader(new FileReader(file));
-			File fout = new File("FPlan_filtered.csv");
+			File fout = new File("data/FPlan_filtered.csv");
 			FileOutputStream fos = new FileOutputStream(fout);		 
 			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos));
 			String line;
 			char[] tmp = new char[14];
+			System.out.println("Processing data...");
 			while ((line = br.readLine()) != null) {
-
 				if(Pattern.compile("^(\\d{7}\\s)").matcher(line).find()){
-					System.out.println(".");
 					line.getChars(0,7,tmp,0);
 					tmp[7] = ',';
 					line.getChars(37,43,tmp,8);
+					if(tmp[8]==' '){
+						continue;
+					}
 					bw.write(tmp);
 					bw.newLine();
 				}		
-			}			
+			}		
+			System.out.println("Finished!");
 			br.close();
 			bw.close();
 		} catch (Exception e){
