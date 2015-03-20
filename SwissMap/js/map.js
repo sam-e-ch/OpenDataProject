37236a6a8d22d8b2e5abb143a8e87bc1cc51db81
GodlikeSpam0r <babemusicstar@hotmail.com>
2015-03-20 16:10:15 +0100

    Now shows the avg last departure per municipality

--- SwissMap/js/map.js
+++ SwissMap/js/map.js
@@ -17,14 +17,40 @@ $(document).ready(function(){
     });
 });
 
-function showMap() {    
+var rateById;
+var quantize
+
+function showMap() {
+    
     var width = $('#map').width(), height = 500;
     var country = d3.geo.path().projection(null);
     var path = d3.geo.path().projection(null);
     var svg = d3.select("#map").append("svg").attr("width", width).attr("height", height).attr("class", "svg");
+    
+    rateById = d3.map();
 
-    //Objects: {municipalities, cantons, districts, country, lakes}
-    d3.json("topo/ch.json", function(error, ch) {
+    quantize = d3.scale.quantize()
+        .domain([900, 2800])
+        .range(d3.range(9).map(function(i) { return "q" + i + "-9";}));
+    
+    
+    d3.json("data/avg.json", function(d) { 
+        for(var i = 0; i<d.length; i++){
+            rateById.set(d[i].id, +d[i].avg);
+        }
+    });
+    
+    
+    d3.json("data/topo/ch.json", function(error, ch) {
+        
+        svg.append("g")
+          .attr("class", "municipalities")
+        .selectAll("path")
+          .data(topojson.feature(ch, ch.objects.municipalities).features)
+        .enter().append("path")
+          .attr("class", function(d) { console.log(d); return quantize(rateById.get(d.id)); })
+          .attr("d", path);
+        
         svg.append("path").datum(topojson.feature(ch, ch.objects.country))
             .attr("class", "country").attr("d", country);
         
@@ -37,9 +63,9 @@ function showMap() {
         
         svg.append("path")
             .datum(topojson.mesh(ch, ch.objects.cantons, function(a, b) { return a !== b; }))
-            .attr("class", "canton-boundaries").attr("d", path);        
+            .attr("class", "canton-boundaries").attr("d", path);
 
-    });    
+    }); 
 }
 
 function toggleHide(cl){
