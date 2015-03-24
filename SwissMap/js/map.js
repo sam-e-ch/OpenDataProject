9fc2e7949617dffdc2398c1210685eea2f24aa0a
GodlikeSpam0r <babemusicstar@hotmail.com>
2015-03-24 10:28:55 +0100

    add tooltips and some refactoring

--- SwissMap/js/map.js
+++ SwissMap/js/map.js
@@ -15,17 +15,17 @@ $(document).ready(function(){
     
      $('input[type=radio][name=show]').change(function() {
         if(this.value == 'municipalities'){
-            $('.cantons').attr('opacity', 0);
-            if ($('.municipalities').attr('opacity'))
-                $('.municipalities').removeAttr('opacity');
+            $('.cantons').attr('display', 'none');
+            if ($('.municipalities').attr('display'))
+                $('.municipalities').removeAttr('display');
             
         } else if (this.value == 'cantons'){
-            $('.municipalities').attr('opacity', 0);
-            if ($('.cantons').attr('opacity'))
-                $('.cantons').removeAttr('opacity');
+            $('.municipalities').attr('display', 'none');
+            if ($('.cantons').attr('display'))
+                $('.cantons').removeAttr('display');
         } else {
-            $('.municipalities').attr('opacity', 0);
-            $('.cantons').attr('opacity', 0);
+            $('.municipalities').attr('display', 'none');
+            $('.cantons').attr('display', 'none');
         }     
      });
     
@@ -43,7 +43,7 @@ function showMap() {
     var path = d3.geo.path().projection(null);
     var svg = d3.select("#map").append("svg").attr("width", width).attr("height", height).attr("class", "svg");
     
-    var rateById = d3.map();
+    var rateMunicipality = d3.map();
     var rateCanton = d3.map();
 
     var quantize = d3.scale.quantize()
@@ -53,7 +53,7 @@ function showMap() {
     
     d3.json("data/avg.json", function(d) { 
         for(var i = 0; i<d.length; i++){
-            rateById.set(d[i].id, +d[i].avg);
+            rateMunicipality.set(d[i].id, +d[i].avg);
         }
     });
     
@@ -64,46 +64,65 @@ function showMap() {
     });
     
     
-    d3.json("data/topo/ch.json", function(error, ch) {
-        
+    d3.json("data/topo/ch.json", function(error, ch) {        
+        drawCountry(ch);        
+        drawMunicipalities(ch);        
+        drawCantons(ch);                
+        drawLakes(ch);   
+        drawCantonBorders(ch);
+    }); 
+    
+    function drawCountry(ch){
         svg.append("path").datum(topojson.feature(ch, ch.objects.country))
             .attr("class", "country").attr("d", country);
-        
-        svg.append("g")
-          .attr("class", "municipalities")
-        .selectAll("path")
-          .data(topojson.feature(ch, ch.objects.municipalities).features)
-        .enter().append("path")
-          .attr("class", function(d) { return quantize(rateById.get(d.id)); })
-          .attr("d", path);
-        
-        svg.append("g")
-          .attr("class", "cantons")
-        .attr("opacity", "0")
-        .selectAll("path")
-          .data(topojson.feature(ch, ch.objects.cantons).features)
-        .enter().append("path")
-          .attr("class", function(d) { return quantize(rateCanton.get(d.id)); })
-          .attr("d", path);
-                
+    }
+    
+    function drawMunicipalities(ch){
+        svg .append("g")
+            .attr("class", "municipalities")
+            .selectAll("path")
+            .data(topojson.feature(ch, ch.objects.municipalities).features)
+            .enter().append("path")
+            .attr("class", function(d) { return quantize(rateMunicipality.get(d.id)); })
+            .attr("d", path)
+            .append("title")
+            .text(function(d){return rateMunicipality.get(d.id);});
+    }
+    
+    function drawCantons(ch){
+        svg .append("g")
+            .attr("class", "cantons")
+            .attr("display", "none")
+            .selectAll("path")
+            .data(topojson.feature(ch, ch.objects.cantons).features)
+            .enter().append("path")
+            .attr("class", function(d) { return quantize(rateCanton.get(d.id)); })
+            .attr("d", path)
+            .append("title")
+            .text(function(d){return rateCanton.get(d.id);});
+    }
+    
+    function drawLakes(ch){
         svg.append("path").datum(topojson.feature(ch, ch.objects.lakes))
-            .attr("class", "lake").attr("d", path);        
-        
-        svg.append("path")
-            .datum(topojson.mesh(ch, ch.objects.municipalities, function(a, b) { return a !== b; }))
-            .attr("class", "municipality-boundaries").attr("d", path);
-        
+            .attr("class", "lake").attr("d", path);     
+    }
+    
+    function drawCantonBorders(ch){
         svg.append("path")
             .datum(topojson.mesh(ch, ch.objects.cantons, function(a, b) { return a !== b; }))
-            .attr("class", "canton-boundaries").attr("d", path);
-
-    }); 
+            .attr("class", "canton-boundaries").attr("d", path);   
+    }
 }
+
 
 function toggleHide(cl){
     if (cl.attr('opacity'))
         cl.removeAttr('opacity');
     else cl.attr('opacity', 0);
+    
+    if (cl.attr('display'))
+        cl.removeAttr('display');
+    else cl.attr('display', 'none');
 }
 
 function resize(){
